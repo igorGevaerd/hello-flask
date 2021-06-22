9ee7eeae3086947ad4173d68651a52b8c75ac4ce
igorGevaerd <igor.gevaerd@gmail.com>
2021-06-22 07:22:44 -0300

    add requirements and fixed small issues

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ name = socket.gethostname()
 
 @app.route('/')
 def hello():
-return "Hello World! \n\n\n From: %s" % name
+  return "Hello World! \n\n\n From: %s" % name
 
 if __name__ == '__main__':
-app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
