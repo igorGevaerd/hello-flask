6ab95356db98a9e50ad6726ff59da74a8461241b
igorGevaerd <igor.gevaerd@gmail.com>
2021-06-22 07:09:23 -0300

    add Helloworld app

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+import socket
+name = socket.gethostname()
+
+@app.route('/')
+def hello():
+return "Hello World! \n\n\n From: %s" % name
+
+if __name__ == '__main__':
+app.run(host='0.0.0.0', debug=True)
