621ba2c3de1b7c23fb00e63d9d56bdd5141c5153
igorGevaerd <igor.gevaerd@gmail.com>
2021-06-24 06:37:37 -0300

    added dockerfile and fixed app

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import socket
 name = socket.gethostname()
 
 @app.route('/')
-def hello():
-  return "Hello World! \n\n\n From: %s" % name
+def hello_world():
+    return 'Hello, World! <br><br><br> From: %s' % name
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
